87231b4ca773ac3ed77153e5e7b2d7033d23a1b9
Dharshini-S12 <118833017+Dharshini-S12@users.noreply.github.com>
2023-10-07 20:44:07 +0530

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function validation(){
     /*var x= document.forms["fform"]["username"].value;*/
-    var x= fform.username.value;
+    /*var x= fform.username.value;
     var lenx= x.length;
     var y= fform.password.value;
     var leny= y.length;
@@ -27,5 +27,36 @@ function validation(){
     else{
         alert("Congragulations! you have submited successfully");
         return false;
+    }*/
+    // Perform authentication and obtain the token
+    const token = "1234"; // Replace with the actual token received from the server
+
+    // Store the token securely in local storage
+    localStorage.setItem('token', token);
+
+    // Redirect to the restricted page upon successful login
+    window.location.href = 'welcome.html';
+
+
+    /*function fetchData() {
+        // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint URL
+        const apiUrl = 'http://127.0.0.1:8000/api/login/';
+
+        // Make an AJAX request using the Fetch API
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                // Handle the data and update the HTML content
+                const contentDiv = document.getElementById('content');
+                contentDiv.innerHTML = data.content; // Assuming the API returns content in a 'content' field
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
     }
+
+    // Call the fetchData function when the page loads
+    window.onload = fetchData;*/
+
+    
 }
